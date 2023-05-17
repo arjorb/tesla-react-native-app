7f628c29ed45fb52d7bde30eca37473d09b07c7c
John U <arjorb@gmail.com>
2023-05-17 20:05:16 +0200

    adding the expo route

--- app/components/MenuOption.js
+++ app/components/MenuOption.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
-
+import { Link } from "expo-router";
 const MenuOption = ({ item }) => {
   return (
     <View style={styles.optionRow}>
       <MaterialCommunityIcons name={item.iconName} size={26} color="gray" />
-      <Text style={styles.optionText}>{item.name}</Text>
+      <Link href={item.href} style={styles.optionText}>
+        {item.name}
+      </Link>
       <MaterialIcons name="keyboard-arrow-right" size={24} color="gray" style={{ marginLeft: "auto" }} />
     </View>
   );
